911b7f6d14e18b6dd252ff10d9d56d9bb4df6c22
Hamza-ck <coderxkider2023@email.com>
2025-04-21 21:11:07 +0530

    Add config.py for Google Sheets connection

---
+++ config.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def connect_sheet(sheet_name):
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file",
+             "https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open(sheet_name)
+    return sheet
