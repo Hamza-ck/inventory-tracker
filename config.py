d7d7660c411853878b8e8b007a2a4291fae71067
Hamza-ck <coderxkider2023@email.com>
2025-04-21 21:20:18 +0530

    Update app.py, config.py to use Streamlit secrets for credentials and theme

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
 
 def connect_sheet(sheet_name):
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/spreadsheets",
-             "https://www.googleapis.com/auth/drive.file",
-             "https://www.googleapis.com/auth/drive"]
-
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    # Use credentials from Streamlit secrets
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["service_account"], scope)
     client = gspread.authorize(creds)
-
     sheet = client.open(sheet_name)
     return sheet
